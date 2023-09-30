ae642fa3d215643966257880cd765177903354d1
ilya <unknown@user.ru>
2023-09-30 17:55:55 +0200

    dark theme redesign and a little optimization of tags slug

--- blog/modules/services.py
+++ blog/modules/services.py
@@ -3,9 +3,13 @@ from time import time
 from django.core.paginator import Paginator
 
 
-def title_to_slug(s):
+def post_slugifier(s):
     slugified = slugify(s, allow_unicode=True)
     return slugified + str(int(time()))
+
+
+def tag_slugifier(title):
+    return slugify(title, allow_unicode=True)
 
 
 def paginate(page_number, queryset, page_size):
