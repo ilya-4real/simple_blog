0c61f5dbe2d86a540c336f736435f601e0006182
ilya <unknown@user.ru>
2023-10-01 18:57:03 +0200

    again solving

--- web/settings.py
+++ web/settings.py
@@ -38,8 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'blog',
     'debug_toolbar',
+    'blog',
     'rest_framework',
     # custom apps
     'api.apps.ApiConfig',
